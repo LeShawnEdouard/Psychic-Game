efd3c9b17116de41542e1e8e5553ac63413141be
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-02 14:51:49 -0500

    Fixed syntax error on line 40 by adding a document.addEventListener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
     var wins = 0; // Shows the user # of wins he/she has earned //
     var losses = 0; // Shows the user # of losses he/she has earned //
     var guesses = 10; // Shows the user # of guesses he/she has //
@@ -9,7 +11,7 @@
     var winTotal = document.getElementById("win-total");
     var lossTotal = document.getElementById("loss-total");
     var guessLeft = document.getElementById("guesses");
-    var computerGuess = document.getElementById("computer-guess");
+    // var computerGuess = document.getElementById("computer-guess");
     var useGuess = document.getElementById("use-guess");
 
     // Function to match keypress with random vowel by the computer //
@@ -22,7 +24,7 @@
                     return randomLetter;
             }
         }
-        
+        var computerGuess = document.getElementById("computer-guess");
         var userGuess = event.key;
         console.log(userGuess)
         winTotal.textContent = wins;
@@ -36,62 +38,8 @@
         // }else {
         //     losses++;
         // }    
+        vls.getVowels();
+        console.log(vls);
+        var computerGuess = vls.getVowels()    
     }
-    vls.getVowels();
-    console.log(vls);
-    var computerGuess = vls.getVowels()
-
-
-
-    
-
-
-
-
-
-
-
-
-
-    // function getRandomVowel() {
-    //     // Used to randomly select a letter to guess //
-    //     var randomLetter = vowels.length[Math.floor(Math.random() * vowels.length)];
-    //     return randomLetter;
-    // }
-    // console.log(getRandomVowel());
-
-    // function gameFunction(randomLetter) {
-    //     winTotal.textContent = wins;
-    //     lossTotal.textContent = losses;
-    //     guessLeft.textContent = guesses;
-    //     if (onkeypress === vowels[i]) {
-    //         wins++;
-    //         guesses--;
-    //     }
-    // }
-    // document.onkeypress = gameFunction;
-    
-
-
-    // document.onkey = function(event) {
-    //     for (i = 0; i < letters.length; i++) {
-    //         if (event.key === [i]) wins++;
-    //     }
-    //     guesses--;
-    //     userText.textContent += event.key;
-    //     guessLeft.textContent = guesses;
-    // }
-
-
-    // // function that holds variables ... //
-    // function gameFunction(event) {
-    //     if (event.keyCode >= 65 && event.keyCode <= 90) {
-    //         alert('This is a test');
-    //     }
-    //     for (var j = 0; j < favTVshows.length; j++) {
-    //         guessLeft = guesses;
-    //         lossTotal = losses;
-    //         winTotal = wins;
-    //     }
-
-    // }
+});
