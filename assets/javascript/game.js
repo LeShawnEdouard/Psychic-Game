c147656eb8bf868bdac24755ff6e4ed9e05635cf
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-01 19:54:37 -0500

    Adding the random function as a variable to perform a composition of functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
     var wins = 0; // Shows the user # of wins he/she has earned //
     var losses = 0; // Shows the user # of losses he/she has earned //
     var guesses = 10; // Shows the user # of guesses he/she has //
-    
+    var compGuess = "";
+    var guessDisplay = "";
+    var computerFunction = vls;
     
 
     // Variables to reference id from DOM //
@@ -9,21 +11,11 @@
     var lossTotal = document.getElementById("loss-total");
     var guessLeft = document.getElementById("guesses");
     var computerGuess = document.getElementById("computer-guess");
-    var userGuess = document.getElementById("user-guess");
+    var useGuess = document.getElementById("use-guess");
 
-    // var vls = {
-    //     vowels: ['a', 'e', 'i', 'o', 'u'], // Array of vowels to chose from //
-    //     getVowels: function(){console.log(this.vowels.length)
-    //         var randomLetter = this.vowels[Math.floor(Math.random() * this.vowels.length)];
-    //             return randomLetter;
-    //     }
-    // }
-    // vls.getVowels();
-    // console.log(vls);
-    // var computerGuess = vls.getVowels()
-
+    // Function to match keypress with random vowel by the computer //
     document.onkeyup = function(event) {
-        // Function to match keypress with random vowel by the computer //
+        
         var vls = {
             vowels: ['a', 'e', 'i', 'o', 'u'], // Array of vowels to chose from //
             getVowels: function(){console.log(this.vowels.length)
@@ -33,17 +25,18 @@
         }
         
         var userGuess = event.key;
-        console.log(userGuess, "userGuess")
-        console.log(computerGuess,"computerGuess")
+        console.log(userGuess)
         winTotal.textContent = wins;
         lossTotal.textContent = losses;
         guessLeft.textContent = guesses;
-        if (event.key === vowels[i]) {
-            wins++;
-            guesses--;
-        }else {
-            losses++;
-        }    
+        computerGuess.textContent = compGuess;
+        useGuess.textContent = guessDisplay;
+        // if (event.key === vowels[i]) {
+        //     wins++;
+        //     guesses--;
+        // }else {
+        //     losses++;
+        // }    
     }
     vls.getVowels();
     console.log(vls);
