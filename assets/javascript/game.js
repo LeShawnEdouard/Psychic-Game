83e3fe632d2504aafce45d0c74edd7a276bc61c6
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-06-12 09:29:15 -0400

    Starting over. Added event handler and created variables for default values when the game begins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,57 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
 
-    var wins = 0; // Shows the user # of wins he/she has earned //
-    var losses = 0; // Shows the user # of losses he/she has earned //
-    var guesses = 10; // Shows the user # of guesses he/she has //
-    var compGuess = "";
-    var guessDisplay = "";
+//     var wins = 0; // Shows the user # of wins he/she has earned //
+//     var losses = 0; // Shows the user # of losses he/she has earned //
+//     var guesses = 10; // Shows the user # of guesses he/she has //
+//     var compGuess = "";
+//     var guessDisplay = "";
     
 
-    // Variables to reference id from DOM //
-    var winTotal = document.getElementById("win-total");
-    var lossTotal = document.getElementById("loss-total");
-    var guessLeft = document.getElementById("guesses");
-    // var computerGuess = document.getElementById("computer-guess");
-    var useGuess = document.getElementById("use-guess");
+//     // Variables to reference id from DOM //
+//     var winTotal = document.getElementById("win-total");
+//     var lossTotal = document.getElementById("loss-total");
+//     var guessLeft = document.getElementById("guesses");
+//     // var computerGuess = document.getElementById("computer-guess");
+//     var useGuess = document.getElementById("use-guess");
 
-    // Function to match keypress with random vowel by the computer //
-    document.onkeyup = function(event) {
+//     // Function to match keypress with random vowel by the computer //
+//     document.onkeyup = function(event) {
         
-        var vls = {
-            vowels: ['a', 'e', 'i', 'o', 'u'], // Array of vowels to chose from //
-            getVowels: function(){console.log(this.vowels.length)
-                var randomLetter = this.vowels[Math.floor(Math.random() * this.vowels.length)];
-                    return randomLetter;
-            }
-        }
-        var computerGuess = document.getElementById("computer-guess");
-        var userGuess = event.key;
-        console.log(userGuess)
-        winTotal.textContent = wins;
-        lossTotal.textContent = losses;
-        guessLeft.textContent = guesses;
-        computerGuess.textContent = compGuess;
-        useGuess.textContent = guessDisplay;
-        // if (event.key === vowels[i]) {
-        //     wins++;
-        //     guesses--;
-        // }else {
-        //     losses++;
-        // }    
-        vls.getVowels();
-        console.log(vls);
-        var computerGuess = vls.getVowels()    
-    }
-});
+//         var vls = {
+//             vowels: ['a', 'e', 'i', 'o', 'u'], // Array of vowels to chose from //
+//             getVowels: function(){console.log(this.vowels.length)
+//                 var randomLetter = this.vowels[Math.floor(Math.random() * this.vowels.length)];
+//                     return randomLetter;
+//             }
+//         }
+//         var computerGuess = document.getElementById("computer-guess");
+//         var userGuess = event.key;
+//         console.log(userGuess)
+//         winTotal.textContent = wins;
+//         lossTotal.textContent = losses;
+//         guessLeft.textContent = guesses;
+//         computerGuess.textContent = compGuess;
+//         useGuess.textContent = guessDisplay;
+//         // if (event.key === vowels[i]) {
+//         //     wins++;
+//         //     guesses--;
+//         // }else {
+//         //     losses++;
+//         // }    
+//         vls.getVowels();
+//         console.log(vls);
+//         var computerGuess = vls.getVowels()    
+//     }
+// });
+
+// Using the "DOMContentLoaded" event handler to start the game upon page load
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Variables used to set our default values when the game begins
+  var wins = 0;
+  var losses = 0;
+  var guesses = 10;
+  var compGuess = "";
+  var userGuess = "";
+
+
+})
