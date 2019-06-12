84507dd5171e6323738e4f98b431c046a6934660
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-06-12 17:33:21 -0400

    Added query selectors to display default values in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,14 +44,36 @@
 //     }
 // });
 
+                        // OLD CODE  ^ //
+// ========================================================================//
+
+
+
 // Using the "DOMContentLoaded" event handler to start the game upon page load
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Variables used to set our default values when the game begins
+// document.addEventListener("DOMContentLoaded", function(event) {
+  // Variables used to set our default values when the game begins  
   var wins = 0;
   var losses = 0;
   var guesses = 10;
   var compGuess = "";
   var userGuess = "";
+  console.log(wins, losses, guesses);
 
+  
+  screenManager()
+  function screenManager() {
+    document.querySelector("#win-total").innerHTML = wins;
+    document.querySelector("#loss-total").innerHTML = losses;
+    document.querySelector("#guesses").innerHTML = guesses;
+    document.querySelector("#computer-guess").innerHTML = compGuess;
+    document.querySelector("#user-guess").innerHTML = userGuess;
+  }
+  
 
-})
+  document.onkeyup = function(event) {
+    
+    // Array that contains the vowels users can select
+    var vowels = ["a", "e", "i", "o", "u"]
+  }
+
+// })
