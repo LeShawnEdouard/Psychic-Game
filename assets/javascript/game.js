4a9f41f1f8e9da0767a9997da267f8a339c3ab21
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-07-10 18:27:31 -0400

    Added if/else statements to onkeyup function. Removed for loop from getcomputerguess function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,57 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
 
-//     var wins = 0; // Shows the user # of wins he/she has earned //
-//     var losses = 0; // Shows the user # of losses he/she has earned //
-//     var guesses = 10; // Shows the user # of guesses he/she has //
-//     var compGuess = "";
-//     var guessDisplay = "";
-    
-
-//     // Variables to reference id from DOM //
-//     var winTotal = document.getElementById("win-total");
-//     var lossTotal = document.getElementById("loss-total");
-//     var guessLeft = document.getElementById("guesses");
-//     // var computerGuess = document.getElementById("computer-guess");
-//     var useGuess = document.getElementById("use-guess");
-
-//     // Function to match keypress with random vowel by the computer //
-//     document.onkeyup = function(event) {
-        
-//         var vls = {
-//             vowels: ['a', 'e', 'i', 'o', 'u'], // Array of vowels to chose from //
-//             getVowels: function(){console.log(this.vowels.length)
-//                 var randomLetter = this.vowels[Math.floor(Math.random() * this.vowels.length)];
-//                     return randomLetter;
-//             }
-//         }
-//         var computerGuess = document.getElementById("computer-guess");
-//         var userGuess = event.key;
-//         console.log(userGuess)
-//         winTotal.textContent = wins;
-//         lossTotal.textContent = losses;
-//         guessLeft.textContent = guesses;
-//         computerGuess.textContent = compGuess;
-//         useGuess.textContent = guessDisplay;
-//         // if (event.key === vowels[i]) {
-//         //     wins++;
-//         //     guesses--;
-//         // }else {
-//         //     losses++;
-//         // }    
-//         vls.getVowels();
-//         console.log(vls);
-//         var computerGuess = vls.getVowels()    
-//     }
-// });
-
-                        // OLD CODE  ^ //
-// ========================================================================//
 
 
 
 // Using the "DOMContentLoaded" event handler to start the game upon page load
 // document.addEventListener("DOMContentLoaded", function(event) {
-  // Variables used to set our default values when the game begins  
-  var wins = 0;
-  var losses = 0;
-  var guesses = 10;
-  var compGuess = "";
-  var userGuess = "";
-  console.log(wins, losses, guesses);
+// Variables used to set our default values when the game begins     
+var wins = 0;
+var losses = 0;
+var livesLeft = 10;
+var compGuess = "";
+var userGuess = "";
 
-  
-  screenManager()
-  function screenManager() {
+var vowels = ["a", "e", "i", "o", "u"]
+console.log(wins, losses, livesLeft);
+
+// This is the flow of events
+
+getComputerGuess() // Update value of compGuess
+screenManager()
+document.onkeyup = function (event) {
+    userGuess = event.key;
+    if(userGuess === compGuess) {
+        wins ++;
+        alert("You have won!!!");
+    }
+    else {
+        livesLeft --;
+    }
+    if(livesLeft < 1) {
+        losses ++;
+        alert("Sorry dude. You suck! The correct letter is" + " " + compGuess)
+    }
+    screenManager() // set variable values to HTML
+}
+
+
+
+// Functions go here
+function screenManager() {
     document.querySelector("#win-total").innerHTML = wins;
     document.querySelector("#loss-total").innerHTML = losses;
-    document.querySelector("#guesses").innerHTML = guesses;
-    document.querySelector("#computer-guess").innerHTML = compGuess;
+    document.querySelector("#lives-left").innerHTML = livesLeft;
+    // document.querySelector("#computer-guess").innerHTML = compGuess;
     document.querySelector("#user-guess").innerHTML = userGuess;
-  }
-  
+    document.querySelector("#already-guessed").innerHTML += userGuess;
+}
 
-  document.onkeyup = function(event) {
-    
-    // Array that contains the vowels users can select
-    var vowels = ["a", "e", "i", "o", "u"]
-  }
+function getComputerGuess() {
+    compGuess = vowels[Math.floor(Math.random() * vowels.length)];
+    console.log("compGuess" , compGuess);
+}
+
+
+
 
 // })
